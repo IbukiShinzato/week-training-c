f4d524777ae60873ad124a53bbf90a3a1aaf40e0
IbukiShinzato <e235718@ie.u-ryukyu.ac.jp>
2026-02-25 17:18:00 +0900

    feat: 練習問題7-5

---
+++ 7/problems/5/logic.c
@@ -0,0 +1,11 @@
+#include "logic.h"
+
+#include <stdlib.h>
+#include <time.h>
+
+int player;
+int computer;
+
+void init() { srand((unsigned)time(NULL)); }
+void setPlayer(int num) { player = num; }
+void setComputer() { computer = rand() % 3; }
